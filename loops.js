4f559e9c4cbc2d11880ddf01c6711a720bf53e92
Chris Li <cli78822@gmail.com>
2018-08-10 05:34:19 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,20 @@
+function forLoop(arr){
+  for(let i = 0 ; i < 25; i++){
+    if(i === 1){
+      arr.push('I am 1 strange loop')
+    }else{
+      arr.push(`I am ${i} strange loops`)
+    }
+  }
+}
+
+function whileLoop(n){
+  while(n>-1){
+    console.log(n)
+  }
+  return 'done'
+}
+
+function doWhile(arr){
+  
+}
